a66f1fb9db0cd2d49360b74bea5a4db5f4c69f81
Ben Haeringer <ben.haeringer@gmail.com>
2019-09-18 20:07:11 +0200

    Psutil integration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,36 @@
+import psutil
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def api_root():
-    return 'foo'
+    """Show available endpoints."""
+
+    return {
+        'processes_url': 'https://<insert domain here>/processes',  # TODO
+    }
+
+
+@app.route('/processes')
+def processes():
+    """List all processes running on the host system."""
+
+    processes = {}
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=[
+                'name', 'pid', 'ppid', 'environ', 'cmdline',
+            ])
+        except psutil.NoSuchProcess:
+            pass
+        else:
+            processes[pinfo['name']] = pinfo
+
+    return processes
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
